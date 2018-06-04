7bd0a76098f842c7c74406ab824b8fcefc9b6976
habouban <39265429+habouban@users.noreply.github.com>
2018-06-04 11:43:19 +0300

    To be corrected due to PT failure

--- FinalProjectDataAnalysis.m
+++ FinalProjectDataAnalysis.m
@@ -1,9 +1,10 @@
+close all
 clear
 
 %% Step 0: Loading Data
 
-P = 10; %-- Number of patients
-N = 14; %-- Number of trials per each patient
+ P   = 15; %-- Number of patients
+ N   = 14; %-- Number of trials per each patient
 
 [Covs, labels_matrix,specific_motion_matrix] = GetData(P, N);
 
@@ -12,7 +13,7 @@ vY   = nan(N*P, 1);
 
 %% Step 1: taking average of all trials for each patient:
 
-Means = {}; %for step 4
+Means  = {}; %for step 4
 
 for pp = 1 : P
     Means{pp} = RiemannianMean(cat(3, Covs{pp,:}));
@@ -23,58 +24,56 @@ mTotalMean = RiemannianMean(cat(3, Means{:}));
 
 %% Step 3: defining Ei
 mSR = mTotalMean^(-1 / 2);
-
 mW  = sqrt(2) * ones(D) - (sqrt(2) - 1) * eye(D);
-
-dd =1 ;
-
-mXt0Vec = [];
-
+dd  =1 ;
+mXtoVec = [];
+mX2     = []';
 
 for pp = 1 : P
     E = mSR * ( mTotalMean / Means{pp} )^(1/2);
-    
     for nn = 1 : N
         G         = E * Covs{pp,nn} * E';
-        mLogG     = logm(G) .* mW;
-        
-        
-        if true(size(mLogG))
-            mX{pp,nn} = triu(mLogG);
-        end
-        
-               mX{pp,nn} = mLogG(triu(true(size(mLogG))));
-               
-            
-
-         vY(dd)       = pp; 
-         dd=dd+1;
-         
-
-    end
-    
+        mLogG     = logm(G) .* mW;     
+%       if true(size(mLogG))
+%          mX{pp,nn} = triu(mLogG);
+%       end
+      % mX{pp,nn} = mLogG(triu(true(size(mLogG))));
+        mX2(:,end+1) = mLogG(triu(true(size(mLogG))));
+        vY(dd)       = pp; 
+        dd           = dd+1;
+    end  
 end    
-
- mXt0Vec = cell2mat(reshape(mX,[1,pp*nn]));
-
-
+ mXtoVec = mX2;
 %% Diffusion Maps:
-mW  = squareform( pdist(mXt0Vec') );
-eps =1000* median(mW(:));
+mW  = squareform( pdist(mXtoVec') );
+eps =100* median(mW(:));
 mK  = exp(-mW.^2 / eps^2);
 mA  = mK ./ sum(mK, 2);
 [mPhi, mLam] = eig(mA);
 
-exp_type_col = labels_matrix(:);
+lables_matrix_trans = labels_matrix';
+exp_type_col = lables_matrix_trans(:);
 
-motion_type = specific_motion_matrix(:);
-
+specific_motion_matrix_Trans = specific_motion_matrix';
+motion_type_col = specific_motion_matrix_Trans(:);
 
 %% Plot:
-figure; hold on; grid on; set(gca, 'FontSize', 16);
+close all
+figure;
+subplot(1,3,1); hold on; grid on; set(gca, 'FontSize', 16); ax(1) = gca;
+%title('Diffusion Maps using Covarianes and Riemannian Metric')
+%scatter3(mPhi(:,2), mPhi(:,3), mPhi(:,4), 100, exp_type_col ,'diamond', 'Fill');
+scatter(mPhi(:,2), mPhi(:,3), 100, exp_type_col ,'diamond', 'Fill');
+
+subplot(1,3,2); hold on; grid on; set(gca, 'FontSize', 16); ax(2) = gca;
 title('Diffusion Maps using Covarianes and Riemannian Metric')
-scatter3(mPhi(:,2), mPhi(:,3), mPhi(:,4), 100, exp_type_col ,'diamond', 'Fill');
-
 %scatter3(mPhi(:,2), mPhi(:,3), mPhi(:,4), 100, vY ,'diamond', 'Fill');
+scatter(mPhi(:,2), mPhi(:,3), 100, vY ,'diamond', 'Fill');
 
-%scatter3(mPhi(:,2), mPhi(:,3), mPhi(:,4), 100, motion_type ,'diamond', 'Fill');
+subplot(1,3,3); hold on; grid on; set(gca, 'FontSize', 16); ax(3) = gca;
+%title('Diffusion Maps using Covarianes and Riemannian Metric')
+%scatter3(mPhi(:,2), mPhi(:,3), mPhi(:,4), 100, motion_type_col ,'diamond', 'Fill');
+scatter(mPhi(:,2), mPhi(:,3), 100, motion_type_col ,'diamond', 'Fill');
+
+%linkprop(ax, {'CameraPosition', 'CameraUpVector'}); 
+linkaxes(ax, 'xy');
